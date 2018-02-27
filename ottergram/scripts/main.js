e3cb1eef80e748b9d8e5f59600ebf7c80ed0f8a2
jnikam66 <jnikam66@gmail.com>
2018-02-26 17:05:30 -0800

    Assignment 2

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -1,58 +1,58 @@
-var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
-var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
-var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var DETAIL_IMAGE_SELECTOR = "[data-image-role='target']";
+var DETAIL_TITLE_SELECTOR = "[data-image-role='title']";
+var THUMBNAIL_LINK_SELECTOR = "[data-image-role='trigger']";
 
 function setDetails(imageUrl, titleText) {
-  'use strict';
+  "use strict";
   var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
-  detailImage.setAttribute('src', imageUrl);
+  detailImage.setAttribute("src", imageUrl);
   var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
   detailTitle.textContent = titleText;
 
 }
 
 function imageFromThumb(thumbnail) {
-  'use strict';
-  return thumbnail.getAttribute('data-image-url');
+  "use strict";
+  return thumbnail.getAttribute("data-image-url");
 }
 
 function titleFromThumb(thumbnail) {
-  'use strict';
-  return thumbnail.getAttribute('data-image-title');
+  "use strict";
+  return thumbnail.getAttribute("data-image-title");
 }
 
 function setDetailsFromThumb(thumbnail) {
-  'use strict';
+  "use strict";
   setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 }
 
 function addThumbClickHandler(thumb) {
-  'use strict';
-  thumb.addEventListener('click', function(event) {
+  "use strict";
+  thumb.addEventListener("click", function(event) {
     event.preventDefault();
     setDetailsFromThumb(thumb);
   });
 }
 
 function getThumbnailsArray() {
-  'use strict';
+  "use strict";
   var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
   var thumbnailArray = [].slice.call(thumbnails);
   return thumbnailArray;
 }
 
 function initializeEvents() {
-  'use strict';
+  "use strict";
   var thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickHandler);
 }
 
 function getCurrentImageIndex() {
-  'use strict';
+  "use strict";
   var thumbnailArray = getThumbnailsArray();
   var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
   for (var i = 0; i < thumbnailArray.length; i++) {
-    if (thumbnailArray[i].getAttribute('data-image-url') == detailImage.getAttribute("src")) {
+    if (thumbnailArray[i].getAttribute("data-image-url") == detailImage.getAttribute("src")) {
       return i;
     }
   }
@@ -60,14 +60,14 @@ function getCurrentImageIndex() {
 }
 
 function next() {
-  'use strict';
+  "use strict";
   var thumbnailArray = getThumbnailsArray();
   var nextImage = (getCurrentImageIndex() + 1) % thumbnailArray.length;
   setDetails(imageFromThumb(thumbnailArray[nextImage]), titleFromThumb(thumbnailArray[nextImage]));
 }
 
 function prev() {
-  'use strict';
+  "use strict";
   var thumbnailArray = getThumbnailsArray();
   var prevImage = (getCurrentImageIndex() - 1 + thumbnailArray.length) % thumbnailArray.length;
   setDetails(imageFromThumb(thumbnailArray[prevImage]), titleFromThumb(thumbnailArray[prevImage]));
